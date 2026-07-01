abe1f64a3ff5764e061969a38e721d328ba0b0cf
4G0NYY <service@agony.ch>
2026-07-01 09:27:34 +0200

    Fix earlier failed build (undeclared global)

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { existsSync } from 'node:fs'
+import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
